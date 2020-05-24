9b563081061a724e67a924e629e787825d012898
Xiyang Yeh <xyyeh@alumni.stanford.edu>
2020-05-23 21:19:30 -0700

    updated create lib

--- create_lib.sh
+++ create_lib.sh
@@ -15,10 +15,38 @@ echo "Creating lib $1"
 
 echo "Updating CMakeLists.txt"
 cd $curr_dir
-sed -i 's/robotics_exp_proj/'$1'/g' CMakeLists.txt
+sed -i 's/exp_proj/'$1'/g' CMakeLists.txt
 
-echo "Setting up /cmake folder"
-cd $dir/cmake
-sed -i 's/robotics_exp_proj/'$1'/g' config.h.in
-sed -i 's/ROBOTICS_EXP_PROJ/'${1^^}'/g' config.h.in
-for file in *.cmake.in ; do mv $file ${file//robotics_exp_proj/"$1"} ; done
+echo "Updating /cmake folder"
+cd $curr_dir/cmake
+sed -i 's/exp_proj/'$1'/g' config.h.in
+sed -i 's/EXP_PROJ/'${1^^}'/g' config.h.in
+for file in *.cmake.in ; do mv $file ${file//exp_proj/"$1"} ; done
+
+echo "Updating /src folder"
+cd $curr_dir/src
+sed -i 's/exp_proj/'$1'/g' exp_class.cc
+
+echo "Updating /include/robotics folder"
+cd $curr_dir/include/robotics
+mv exp_proj $1
+cd $1
+sed -i 's/exp_proj/'$1'/g' exp_class.h
+sed -i 's/EXP_PROJ/'${1^^}'/g' exp_class.h
+
+echo "Updating /apps folder"
+cd $curr_dir/apps
+sed -i 's/exp_proj/'$1'/g' CMakeLists.txt
+sed -i 's/exp_proj/'$1'/g' app.cc
+sed -i 's/EXP_PROJ/'${1^^}'/g' app.cc
+
+echo "Updating /test folder"
+cd $curr_dir/test
+sed -i 's/exp_proj/'$1'/g' exp_class_test.cc
+
+echo "Updating Doxyfile"
+cd $curr_dir
+str=`echo -e ${1//_/ } | sed -r 's/\<./\U&/g'`
+sed -i "s/Exp Proj/${str}/g" Doxyfile
+
+cd $curr_dir
